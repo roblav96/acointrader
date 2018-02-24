20fd8bf99e6fd450d705ec9a37ab2341a9f68977
Robert Laverty <roblav96@gmail.com>
2018-02-24 15:56:45 -0500

    cleanup

--- server/types/server.globals.d.ts
+++ server/types/server.globals.d.ts
@@ -1,9 +1,6 @@
 // 
 
-import * as restify from 'restify'
-import * as errors from 'restify-errors'
 import * as axios from 'axios'
-import * as forge from 'node-forge'
 
 
 
@@ -28,31 +25,12 @@ declare global {
 		format(c: any): any
 	}
 
-
-
 	interface AxiosError extends axios.AxiosError {
 		config?: axios.AxiosRequestConfig
 		response?: AxiosErrorResponse
 	}
 	interface AxiosErrorResponse extends axios.AxiosResponse {
 		data: any
-	}
-	type AxiosCanceler = axios.Canceler
-	type IdkError = Error & AxiosError & errors.HttpError
-
-
-
-	interface SecurityDoc {
-		ip: string
-		conn: string
-		uuid: string
-		finger: string
-		email: string
-		prime: string
-		token: string
-		publicKey: string
-		privateKey: string
-		authed: boolean
 	}
 
 
